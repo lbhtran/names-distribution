8d92c9aed31f6a0b55be1efba9ecc1d96e2f3b46
Alexx <lbhtran@gmail.com>
2019-02-04 18:52:14 +0000

    adding admin interface: minor clean up, somewhere I have broke flask-admin :(

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, g
 from flask_login import current_user, login_required
 from flask_babel import _, get_locale
 from app import db
-from app.models import User, Refugee
+from app.models import User
 from app.main import bp
 
 @bp.before_app_request
